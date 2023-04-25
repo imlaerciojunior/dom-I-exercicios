6cde5558d327cfb3734be0a0a622413b2d59b358
imlaerciojunior <imlaerciojunior@gmail.com>
2023-04-25 20:29:49 -0300

    resolução dos exercicios de DOM

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 const frutas = ["laranja", "limão", "uva"]
+
+//EXERCICIO 1 
+
+const fruta1 = document.getElementById("fruta-1")
+fruta1.innerHTML += frutas[0]
+
+const fruta2 = document.getElementById("fruta-2")
+fruta2.innerHTML += frutas[1]
+
+const fruta3 = document.getElementById("fruta-3")
+fruta3.innerHTML += frutas[2]
+
+// EXERCICIO 2
+
+const novaFruta = document.getElementById("fruta")
+console.log(novaFruta.value)
+
+//EXERCICIO 3
+
+const proximaFruta = () => {
+    const fruta4 = document.getElementById("fruta-4")
+    const novaFruta = document.getElementById("fruta")
+    fruta4.innerHTML = novaFruta.value
+
+    novaFruta.value = ''
+}
+
